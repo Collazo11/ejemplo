d27b5886a1fee904eb3a1a0d29a9662448e0f419
jnavacol <jjnc.tics@gmail.com>
2021-11-08 14:18:57 -0600

    actualizaciones

--- src/com/company/Clientes.java
+++ src/com/company/Clientes.java
@@ -28,11 +28,6 @@ class ClientesCatalog {
             else
                 JOptionPane.showMessageDialog(null, "The item number you entered is not valid");
 
-
-
-
-
-
         //manejo de clientes
         String name;
         ClientesCatalog catCtes = new ClientesCatalog();
@@ -54,5 +49,74 @@ class ClientesCatalog {
             System.out.print(arreglo.getCtes(x)+"\n");
         System.out.println();
         }
+
+        //manejo de descuentos a los clientes
+        int[] discount = {0,0,0,0};
+        int[] discount_values = {0,0,0,0};
+        discount_values[0] = 10;
+        discount_values[1] = 20;
+        discount_values[2] = 30;
+        discount_values[3] = 40;
+        for(x=0;x<Num_catCtes;++x){
+            System.out.println("Ingrese el descuento del cliente >> ");
+            try {
+                discount[x] = Integer.parseInt(br.readLine());
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\nDescuentos de los clientes");
+        for(x=0;x<Num_catCtes;++x){
+            System.out.print(discount[x]+"\n" + arreglo.getCtes(x)+"\n");
+        }
+        System.out.println();
+
+
+        //dar de  alta un nuevo cliente junto a su compra
+        String new_name;
+        int new_discount;
+        int new_itemOrdered = 0;
+        double new_itemPrice=0.0;
+        boolean isValidNewItem=false;
+
+        System.out.println("\nIngrese el nombre del nuevo cliente >> ");
+        try {
+
+            new_name = br.readLine();
+            arreglo.setCtesNames(Num_catCtes,new_name);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nIngrese el descuento del nuevo cliente >> ");
+        try {
+            new_discount = Integer.parseInt(br.readLine());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nIngrese el item del nuevo cliente >> ");
+        try {
+            new_itemOrdered = Integer.parseInt(br.readLine());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int y=0; y<Number_OF_ITEMS;++y){
+            if (new_itemOrdered == validValues[y]){
+                isValidNewItem = true;
+                new_itemPrice = prices[y];
+
+
+            }
+
+        }
+        if (isValidNewItem)
+            JOptionPane.showMessageDialog(null, "The price of the item : " + new_itemOrdered + "is $" + new_itemPrice);
+            else
+                JOptionPane.showMessageDialog(null, "The item number you entered is not valid");
+
     }
+
 }
--- src/com/company/arreglo.java
+++ src/com/company/arreglo.java
@@ -5,15 +5,17 @@ package com.company;
 public class arreglo {
     private String ctesNames;
     private static String[] ctes = new String[4];
-    private String CtesName;
+    private static String CtesName;
+
 
     public void setCtesNames(String ctes) {
         ctesNames = ctes;
     }
-
-    public static void setCtesNames(int numeber, String name) {
-        ctes[numeber] = name;
+    public static void setCtesNames(int number, String name) {
+        ctes[number] = name;
     }
+
+
 
     public static String getCtes(int numeber) {
         return ctes[numeber];
